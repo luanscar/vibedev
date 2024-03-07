f8ecdfaeaef8cafb9407c8e9e51d159e0f25e389
Luan Araujo <luaanscar@gmail.com>
2024-03-07 20:17:56 -0300

    WIP

---
+++ src/app/app/_components/user-info.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+const UserInfo = (props: Props) => {
+  return <div>UserInfo</div>;
+};
+
+export default UserInfo;
+
+git config --global user.email "luaanscar@gmail.com"
+git config --global user.name "Luan Araujo"
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,10 +1,28 @@
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { auth } from '@/services/auth'
+import { signOut } from 'next-auth/react'
 import React from 'react'
 
 type Props = {}
 
-const Page = (props: Props) => {
+const Page = async (props: Props) => {
+  const session = await auth()
   return (
-    <div>Page</div>
+    <main className='flex items-center justify-center h-screen' >
+      <div className='flex flex-col items-center justify-center space-y-4'>
+        <Avatar>
+          <AvatarFallback>
+            US
+          </AvatarFallback>
+        </Avatar>
+        <span>{session?.user?.email}</span>
+
+        <Button onClick={() => signOut()}>
+          Sign Out
+        </Button>
+      </div>
+    </main>
   )
 }
 
